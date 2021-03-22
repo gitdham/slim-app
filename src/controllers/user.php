7284bc8325754876fff2bff173d5f34ca63bb314
adzanidham <adzani234@gmail.com>
2021-03-22 22:22:35 +0700

    jwt works

--- src/controllers/user.php
+++ src/controllers/user.php
@@ -141,24 +141,20 @@ class User {
     // unset password from $user
     unset($user['password']);
 
-    $secret_key = "supersecrectkey";
-    $issuer_claim = "THE_ISSUER"; // this can be the servername
-    $audience_claim = "THE_AUDIENCE";
-    $issuedat_claim = time(); // issued at
-    $notbefore_claim = $issuedat_claim + 10; //not before in seconds
-    $expire_claim = $issuedat_claim + 60; // expire time in seconds
-    $token = array(
-      "iss" => $issuer_claim,
-      "aud" => $audience_claim,
-      "iat" => $issuedat_claim,
-      "nbf" => $notbefore_claim,
-      "exp" => $expire_claim,
-      "data" => $user
-    );
+    $key = 'privatekey';
+    $iat = time();
+    $exp = $iat + 60 * 60;
+    $payload = [
+      'iss' => 'http://localhost:8080',
+      'aud' => 'http://localhost:8080',
+      'iat' => $iat,
+      'exp' => $exp,
+      'data' => $user
+    ];
 
-    $jwt = JWT::encode($token, $secret_key);
-    $jwt_decoded = JWT::decode($jwt, "supersecrectkey", ['HS256']);
-    var_dump($token);
+    $jwt = JWT::encode($payload, $key);
+    $jwt_decoded = JWT::decode($jwt, $key, ['HS256']);
+    var_dump($payload);
     var_dump($jwt);
     var_dump($jwt_decoded);
     die;
